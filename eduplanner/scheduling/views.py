6bd59578ab212ec9e6330ecb06464e6ff303794b
Jefin2005 <jefin3889@gmail.com>
2026-01-05 22:36:42 +0530

    making reference from cllg timetable

--- eduplanner/scheduling/views.py
+++ eduplanner/scheduling/views.py
@@ -1,3 +1,13 @@
+TIME_SLOTS = [
+    "8:45-9:35",
+    "9:35-10:25",
+    "10:35-11:30",
+    "11:30-12:15",
+    "1:05-1:55",
+    "2:05-2:55",
+    "2:55-3:45",
+]
+
 from django.shortcuts import render
 from academics.models import Faculty, Subject, Classroom
 from .models import TimetableEntry
@@ -6,33 +16,33 @@ from ga_engine.ga_runner import run_ga
 def dashboard(request):
     return render(request, "dashboard.html")
 
+from collections import defaultdict
+from django.shortcuts import render
+from .models import TimetableEntry
+
 def generate_timetable(request):
-    TimetableEntry.objects.all().delete()
+    entries = TimetableEntry.objects.all()
 
     days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-    periods_per_day = 4
 
-    subjects = list(Subject.objects.all())
-    faculties = list(Faculty.objects.all())
-    rooms = list(Classroom.objects.all())
+    timetable = defaultdict(dict)
 
-    best_timetable = run_ga(
-        days=days,
-        periods=periods_per_day,
-        subjects=subjects,
-        faculties=faculties,
-        rooms=rooms
-    )
+    for entry in entries:
+        timetable[entry.day][entry.period] = entry.subject.name
 
-    for entry in best_timetable.entries:
-        TimetableEntry.objects.create(
-            day=entry["day"],
-            period=entry["period"],
-            subject=entry["subject"],
-            faculty=entry["faculty"],
-            classroom=entry["room"]
-        )
+    context = {
+        "days": days,
+        "time_slots": [
+            "8:45-9:35",
+            "9:35-10:25",
+            "10:35-11:30",
+            "11:30-12:15",
+            "1:05-1:55",
+            "2:05-2:55",
+            "2:55-3:45",
+        ],
+        "timetable": timetable,
+    }
 
-    return render(request, "timetable.html", {
-        "entries": TimetableEntry.objects.all()
-    })
+    return render(request, "timetable.html", context)
+
