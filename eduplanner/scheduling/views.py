a4a80fde6b98cd19bbdd4dd8a43939364d18aa63
Jefin2005 <jefin3889@gmail.com>
2026-01-05 22:50:50 +0530

    ..

--- eduplanner/scheduling/views.py
+++ eduplanner/scheduling/views.py
@@ -1,21 +1,3 @@
-TIME_SLOTS = [
-    "8:45-9:35",
-    "9:35-10:25",
-    "10:35-11:30",
-    "11:30-12:15",
-    "1:05-1:55",
-    "2:05-2:55",
-    "2:55-3:45",
-]
-
-from django.shortcuts import render
-from academics.models import Faculty, Subject, Classroom
-from .models import TimetableEntry
-from ga_engine.ga_runner import run_ga
-
-def dashboard(request):
-    return render(request, "dashboard.html")
-
 from collections import defaultdict
 from django.shortcuts import render
 from .models import TimetableEntry
@@ -24,6 +6,15 @@ def generate_timetable(request):
     entries = TimetableEntry.objects.all()
 
     days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    time_slots = [
+        "8:45-9:35",
+        "9:35-10:25",
+        "10:35-11:30",
+        "11:30-12:15",
+        "1:05-1:55",
+        "2:05-2:55",
+        "2:55-3:45",
+    ]
 
     timetable = defaultdict(dict)
 
@@ -32,17 +23,9 @@ def generate_timetable(request):
 
     context = {
         "days": days,
-        "time_slots": [
-            "8:45-9:35",
-            "9:35-10:25",
-            "10:35-11:30",
-            "11:30-12:15",
-            "1:05-1:55",
-            "2:05-2:55",
-            "2:55-3:45",
-        ],
+        "time_slots": time_slots,
+        "period_range": range(1, len(time_slots) + 1),  # ✅ IMPORTANT
         "timetable": timetable,
     }
 
     return render(request, "timetable.html", context)
-
