6b5792340a1b134fb58ae00d8418219a71e26306
Jefin2005 <jefin3889@gmail.com>
2026-01-05 23:24:58 +0530

    recess added

--- eduplanner/scheduling/views.py
+++ eduplanner/scheduling/views.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
 from django.shortcuts import render
 from .models import TimetableEntry
-
 def dashboard(request):
     return render(request, "dashboard.html")
 def generate_timetable(request):
@@ -9,26 +8,28 @@ def generate_timetable(request):
 
     days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 
+    # Slots only for DISPLAY
     time_slots = [
         "8:45-9:35",
         "9:35-10:25",
+        "RECESS",
         "10:35-11:30",
         "11:30-12:15",
-        "LUNCH",              # 👈 lunch marker
+        "LUNCH",
         "1:05-1:55",
+        "RECESS",
         "2:05-2:55",
         "2:55-3:45",
     ]
 
+    # Store only teaching periods (1–7)
     timetable = defaultdict(dict)
-
     for entry in entries:
         timetable[entry.day][entry.period] = entry.subject.name
 
     context = {
         "days": days,
         "time_slots": time_slots,
-        "period_range": range(1, len(time_slots) + 1),
         "timetable": timetable,
     }
 
