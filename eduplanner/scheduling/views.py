cbb1838fee8ceaca7cd92737e11cfa8c9d31c01e
Jefin2005 <jefin3889@gmail.com>
2026-01-05 23:13:06 +0530

    llunch break added

--- eduplanner/scheduling/views.py
+++ eduplanner/scheduling/views.py
@@ -4,16 +4,17 @@ from .models import TimetableEntry
 
 def dashboard(request):
     return render(request, "dashboard.html")
-
 def generate_timetable(request):
     entries = TimetableEntry.objects.all()
 
     days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
     time_slots = [
         "8:45-9:35",
         "9:35-10:25",
         "10:35-11:30",
         "11:30-12:15",
+        "LUNCH",              # 👈 lunch marker
         "1:05-1:55",
         "2:05-2:55",
         "2:55-3:45",
@@ -27,7 +28,7 @@ def generate_timetable(request):
     context = {
         "days": days,
         "time_slots": time_slots,
-        "period_range": range(1, len(time_slots) + 1),  # ✅ IMPORTANT
+        "period_range": range(1, len(time_slots) + 1),
         "timetable": timetable,
     }
 
