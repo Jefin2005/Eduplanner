014c9443889385e715d70ec4fbd307ece831511a
Jefin2005 <jefin3889@gmail.com>
2026-01-06 21:10:26 +0530

    editing dashboard

--- eduplanner/academics/models.py
+++ eduplanner/academics/models.py
@@ -1,32 +1,55 @@
 from django.db import models
 
-class Faculty(models.Model):
-    CATEGORY_CHOICES = [
-        ("PROFESSOR", "Professor"),
-        ("ASSOCIATE", "Associate Professor"),
-        ("ASSISTANT", "Assistant Professor"),
-    ]
-
-    name = models.CharField(max_length=100)
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
-    max_hours = models.IntegerField()
+class College(models.Model):
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+
+
+class Department(models.Model):
+    college = models.ForeignKey(College, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Semester(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    number = models.IntegerField()
+
+    def __str__(self):
+        return f"Semester {self.number}"
+
+
+class ClassRoomGroup(models.Model):
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    section_name = models.CharField(max_length=10)  # A, B, C
+
+    def __str__(self):
+        return f"{self.semester} - {self.section_name}"
+
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=100)
+    max_hours = models.IntegerField()
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 
 
 class Subject(models.Model):
-    SUBJECT_TYPE = [
-        ("THEORY", "Theory"),
-        ("LAB", "Lab"),
-    ]
-
     name = models.CharField(max_length=100)
-    subject_type = models.CharField(max_length=10, choices=SUBJECT_TYPE)
     weekly_hours = models.IntegerField()
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    faculty = models.ForeignKey(Faculty, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
+
 
 
 class Classroom(models.Model):
