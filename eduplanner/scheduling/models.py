b0a7d553760c2f6cc96e9903a9c1ac9b8581be77
Fathima Nazrin <nazrin4762@gmail.com>
2026-01-06 21:27:33 +0530

    ...

--- eduplanner/scheduling/models.py
+++ eduplanner/scheduling/models.py
@@ -1,13 +1,30 @@
 from django.db import models
-from academics.models import Faculty, Subject, Classroom
+from academics.models import Department, Subject, Faculty, Classroom
+
 
 class TimetableEntry(models.Model):
     day = models.CharField(max_length=10)
     period = models.IntegerField()
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
-    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE
+    )
+
+    subject = models.ForeignKey(
+        Subject,
+        on_delete=models.CASCADE
+    )
+
+    faculty = models.ForeignKey(
+        Faculty,
+        on_delete=models.CASCADE
+    )
+
+    classroom = models.ForeignKey(
+        Classroom,
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
-        return f"{self.day} - {self.subject}"
-
+        return f"{self.day} - Period {self.period}"
